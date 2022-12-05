d2bca18bf2d90e00e3417472e1623b2a653844b9
Edgar figueroa <edgar.figueroa.gtz@gmail.com>
2022-12-05 02:12:40 +0000

    Align header items

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <div className='container-app'>
         <header className='header' >
-        <img src="./src/assets/logo-Marelyn.png" alt="logo-marelyn" className='logo' />
-        <h2>Zapateria Marelyn</h2>
+          <img src="./src/assets/logo-Marelyn.png" alt="logo-marelyn" className='logo' />
+          <h2>Zapateria Marelyn</h2>
         </header>
         <Login></Login>
       </div>
