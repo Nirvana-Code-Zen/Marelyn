19cc82b50e1fe9b2cd48954e90d2b444af4609de
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2022-12-22 21:32:27 -0600

    Added jest testing-library, husky and lint staged

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
 import '../Login/Login.css'
 
+export default function Login () {
+  const btn = document.querySelector('.btn-entrar')
+  const btnEntrar = () => {
+    btn.addEventListener('click', () => {
+      console.log('Soy un botn ')
+    })
+  }
 
-
-export default function Login(){
-    const btn = document.querySelector('.btn-entrar')
-    const btnEntrar = ()=>{
-      btn.addEventListener('click',()=>{
-        console.log('Soy un botn ')
-      })
-    }
-    return <div className="form-login">
+  return <div className="form-login">
      <h4>Login</h4>
      <form>
        <div className="input-login">
@@ -19,14 +17,14 @@ export default function Login(){
        </div>
        <div className="input-login">
          <label className='text-form'>Contraseña </label>
-         <input type="password" name="password" required  className='inputs'/>
+         <input type="password" name="password" required className='inputs'/>
        </div>
        <label> <a href="">Olvidaste tu contraseña?</a> </label>
        <div>
        </div>
        <div className="btns">
           <input type="submit" value='Crear usuario' className='btn-create' onClick={btnEntrar} />
-          <input type="submit" value='Entrar'  className='btn-entrar' onClick={btnEntrar}/>
+          <input type="submit" value='Entrar' className='btn-entrar' onClick={btnEntrar}/>
         </div>
      </form>
    </div>
