2b01f50b5e9838210e9032fc379f200429bb0410
Nancy Gonzalez <nancy.glez.dea@gmail.com>
2022-12-29 23:33:43 -0600

    Fix sign up path

--- src/Components/Button/ButtonStyled.js
+++ src/Components/Button/ButtonStyled.js
@@ -3,7 +3,7 @@ import style from 'styled-components'
 export default style.button`
   padding: .3rem 1rem;
   background: ${props => props.theme.colors.secondary};
-  color: #FFF;
+  color: '#fff';
   border-radius: 3px;
   border: none;
   outline: none;
