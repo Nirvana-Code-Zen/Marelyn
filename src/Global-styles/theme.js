f3499b4963ec82fd5947ae5a2e2f954030dd726b
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2023-01-02 14:11:55 -0600

    Sign options ui

--- src/Global-styles/theme.js
+++ src/Global-styles/theme.js
@@ -1,18 +1,14 @@
 export default {
   colors: {
-    primary: '#a78d48',
-    primary_complement: '#FFDF87',
-    primary_second_complement: '#E0BE60',
-    secondary: '#223B94',
-<<<<<<< HEAD
-    secondary_complement: '#607DE0',
-    black: '#111'
-=======
-    secondary_complement: '#607DE0'
->>>>>>> 0e5a82a (Login page (#8))
+    primary: "#a78d48",
+    primary_complement: "#FFDF87",
+    primary_second_complement: "#E0BE60",
+    secondary: "#223B94",
+    secondary_complement: "#607DE0",
+    black: "#111",
   },
   fonts: {
     text: '"Nanum Gothic", sans-serif',
-    titles: '"Nanum Gothic", sans-serif'
-  }
-}
+    titles: '"Nanum Gothic", sans-serif',
+  },
+};
--- src/Pages/SignupOptions/index.test.js
+++ src/Pages/SignupOptions/index.test.js
@@ -1,59 +1,59 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import { useLocation } from 'wouter'
-import firebase from 'firebase/auth'
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useLocation } from "wouter";
+import firebase from "firebase/auth";
 
-import SignUpOptions from './index'
-import { ThemeProvider } from 'styled-components'
+import SignUpOptions from "./index";
+import { ThemeProvider } from "styled-components";
 
-import theme from '../../Global-styles/theme'
+import theme from "../../Global-styles/theme";
 
-jest.mock('wouter', () => {
-  const mockLocation = '/sign-up/'
-  const mockUseLocation = jest.fn()
+jest.mock("wouter", () => {
+  const mockLocation = "/sign-up/";
+  const mockUseLocation = jest.fn();
   return {
-    useLocation: () => [mockLocation, mockUseLocation]
-  }
-})
+    useLocation: () => [mockLocation, mockUseLocation],
+  };
+});
 
-jest.mock('firebase/auth', () => ({
+jest.mock("firebase/auth", () => ({
   GoogleAuthProvider: jest.fn(),
   FacebookAuthProvider: jest.fn(),
   getAuth: jest.fn(),
-  signInWithPopup: jest.fn()
-}))
+  signInWithPopup: jest.fn(),
+}));
 
-describe('<SignUpOptions />', () => {
+describe("<SignUpOptions />", () => {
   beforeEach(() => {
     render(
       <ThemeProvider theme={theme}>
         <SignUpOptions />
       </ThemeProvider>
-    )
-  })
+    );
+  });
 
-  test('Should render Google button ', async () => {
-    const facebookBtn = screen.getByText('Registrate con Facebook')
-    const facebook = jest.spyOn(firebase, 'FacebookAuthProvider')
+  test("Should render Google button ", async () => {
+    const facebookBtn = screen.getByText("Registrate con Facebook");
+    const facebook = jest.spyOn(firebase, "FacebookAuthProvider");
 
-    fireEvent.click(facebookBtn)
-    expect(facebook).toHaveBeenCalled()
-  })
+    fireEvent.click(facebookBtn);
+    expect(facebook).toHaveBeenCalled();
+  });
 
-  test('Should render Facebook button ', () => {
-    const googlebutton = screen.getByText('Registrate con Google')
-    const google = jest.spyOn(firebase, 'GoogleAuthProvider')
+  test("Should render Facebook button ", () => {
+    const googlebutton = screen.getByText("Registrate con Google");
+    const google = jest.spyOn(firebase, "GoogleAuthProvider");
 
-    fireEvent.click(googlebutton)
+    fireEvent.click(googlebutton);
 
-    expect(google).toHaveBeenCalled()
-  })
+    expect(google).toHaveBeenCalled();
+  });
 
-  test('Should render Email button ', () => {
-    const mockUseLocation = useLocation()[1]
-    const btn = screen.getByText('Registrate con Email')
-    fireEvent.click(btn)
+  test("Should render Email button ", () => {
+    const mockUseLocation = useLocation()[1];
+    const btn = screen.getByText("Registrate con Email");
+    fireEvent.click(btn);
 
-    expect(mockUseLocation).toHaveBeenCalled()
-    expect(mockUseLocation).toHaveBeenCalledWith('/sign-up/email')
-  })
-})
+    expect(mockUseLocation).toHaveBeenCalled();
+    expect(mockUseLocation).toHaveBeenCalledWith("/sign-up/email");
+  });
+});
