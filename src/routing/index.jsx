7ba964cdad4c9f33583d25dd61615f7c4bbb00e8
Edgar figueroa <edgar.figueroa.gtz@gmail.com>
2023-01-29 14:37:36 -0600

    Added react context and protected routes (#11)

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -1,38 +1,45 @@
-import { Route, Switch } from 'wouter'
+import { Route } from 'wouter'
+import User from '../Context/User'
 
 import Home from '../Pages/Home'
 import SingUp from '../Pages/Singup'
 import Scope from '../routing/Scope.jsx'
+import ProtectedRoute from './PrivateRoute'
 
-const Routing = () => {
-  return (
-    <Switch>
-      <Scope base='/'>
-        <Route path='/' component={Home} />
-        <Route path="sign-up" component={SingUp} />
-        <Route path="login" />
-        <Route path="restore-password" />
-      </Scope>
-      <Scope base='/products/'>
-        <Route path="create" />
-        <Route path="list" />
-        <Route path="update" />
-        <Route path="categories" />
-        <Route path="sales" />
-        <Route path=":productid/deteils" />
-      </Scope>
-      <Scope base='/reports/'>
-        <Route path="sales" />
-        <Route path="purchases" />
-        <Route path="incomes" />
-      </Scope>
-      <Scope base='resources'>
-        <Route path="clients" />
-        <Route path="providers" />
-      </Scope>
-      <Route path="/orders" />
-    </Switch>
-  )
-}
+const Routing = () => (
+  <>
+    <Route path='/' component={Home} />
+    <Route path="/sign-up" component={SingUp} />
+    <Route path="/login" />
+    <Route path="/restore-password" />
+    <User>
+      <ProtectedRoute>
+        <Scope base='/products'>
+          <Route path="/" component={() => <h1>Products Home</h1>}/>
+          <Route path="/create" component={() => <h1>Products Create</h1>}/>
+          <Route path="/list" />
+          <Route path="/update" />
+          <Route path="/categories" />
+          <Route path="/sales" />
+          <Route path="/:productid/deteils" />
+        </Scope>
+        <Scope base='/reports'>
+          <Route path="/" component={() => <h1>reports Home</h1>}/>
+          <Route path="/sales" />
+          <Route path="/purchases" />
+          <Route path="/incomes" />
+        </Scope>
+        <Scope base='/resources'>
+          <Route path="/" component={() => <h1>Resources Home</h1>}/>
+          <Route path="/clients" />
+          <Route path="/providers" />
+        </Scope>
+        <Scope base='/orders'>
+          <Route path="/" component={() => <h1>Orders Home</h1>}/>
+        </Scope>
+      </ProtectedRoute>
+    </User>
+  </>
+)
 
 export default Routing
