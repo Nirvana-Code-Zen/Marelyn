b87289f85e811af4df308d8796543a48ce510846
Edgar figueroa <edgar.figueroa.gtz@gmail.com>
2023-09-27 18:06:18 -0600

    Fix Signup test, update the snapshots styles (#16)
    Also I added firebase/storage mock to prevent the test failing

--- setupTest.js
+++ setupTest.js
@@ -25,10 +25,15 @@ jest.mock('firebase/auth', () => ({
   GoogleAuthProvider: jest.fn(),
   FacebookAuthProvider: jest.fn(),
   getAuth: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn().mockResolvedValue(),
   signInWithPopup: jest.fn().mockReturnValue({
     user: {
       displayName: 'Jhon Doe',
       email: 'jhon@doe.com'
     }
   })
+}))
+
+jest.mock('firebase/storage', () => ({
+  getStorage: jest.fn()
 }))
