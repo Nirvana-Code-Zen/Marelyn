d48b7a31793e7e62131e1b2fc9a552b7bb0f25b0
Nancy-GDA <nancy.glez.dea@gmail.com>
2023-11-19 15:30:53 -0600

    iModify styles for login page

--- src/UI/Pages/Login/index.tsx
+++ src/UI/Pages/Login/index.tsx
@@ -7,6 +7,7 @@ export const Login = () => {
         <img src="src/UI/assets/Bota.jpeg" alt="banner" className='banner' />
       </ContainerBannerStyled>
       <ContainerFormLoginStyled>
+        
       </ContainerFormLoginStyled>
     </LoginStyled>
   )
