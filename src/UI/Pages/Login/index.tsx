0d47c7b14d1c409dc514ccbc2fa3ec7fd3c80191
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2023-11-20 03:04:18 -0600

    modify facebook button text

--- src/UI/Pages/Login/index.tsx
+++ src/UI/Pages/Login/index.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from '~UI/Context/Auth'
 
 export const Login = () => {
   const auth = useContext(AuthContext)
+
   const login = () => {
     auth.signInWithFacebook()
   }
@@ -15,7 +16,7 @@ export const Login = () => {
         <img src="src/UI/assets/Bota.jpeg" alt="banner" className='banner' />
       </ContainerBannerStyled>
       <ContainerFormLoginStyled>
-      <button onClick={login}>Login</button>
+      <button onClick={login}>Login with Facebook</button>
         
       </ContainerFormLoginStyled>
     </LoginStyled>
